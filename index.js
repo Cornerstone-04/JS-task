b2317d926b7d5f30983db31d49125438f55f834b
Ephraim Cornerstone <fortunecornerstone@gmail.com>
2021-04-01 15:32:50 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// let paraLength = document.getElementsByTagName('p').length;
+// console.log(paraLength);
+
+function myFunction() {
+    var str = "Cornerstone";
+    var num = str.length
+    document.getElementById('display').innerHTML = 'Number of letters \= ' + num;
+   
+  // var myString = 'oeoe'
+    // var numOfVowels = myString.length
+    // document.getElementById('vowels').innerHTML = 'Number of vowels \= ' + numOfVowels;
+    // var myCon = 'crnrstn'
+    // var numOfCon = myCon.length
+    // document.getElementById('consonants').innerHTML = 'Number of consonants \= ' + numOfCon;
+}
