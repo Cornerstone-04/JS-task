181389818d974267c4d97ec295df6676cfa9f931
Ephraim Cornerstone <fortunecornerstone@gmail.com>
2021-04-06 13:44:17 +0100

    Correction

--- index.js
+++ index.js
@@ -1,35 +1,32 @@
-function myFunction() {
-  var str = "Cornerstone";
-  var n = str.length;
- 
-}
-
 function go() {
-
   var str=document.getElementById("t1").value;
-  var vowel="aeiouAEIOU";
-  var consonant="bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ";
-  var ctr=0;
-  var con=0;
- 
-
-  for(var x=0;x<str.length;x++){
-      for(var y=0;y<vowel.length;y++){
-        if(str[x]==vowel[y])
-        {
-            ctr++;
-        } 
-      }
-  }
-  for(var x=0;x<consonant.length;x++){
-      for(var z=0;z<consonant.length;z++){
-          if(str[x]==consonant[z])
-          {
-              con++;
-          }
-      }
-  }
-  document.getElementById("p0").innerHTML="Numbers of characters \= " + str.length;
-  document.getElementById("p1").innerHTML="Numbers of vowels \= " + ctr;
-  document.getElementById("p2").innerHTML="Numbers of consonants \= " + con;
+  document.getElementById("p0").innerHTML="The Numbers of characters " + str.length;
+  document.getElementById("p1").innerHTML="The Numbers of Vowels " + countVowels(str);
+  document.getElementById("p2").innerHTML="The Numbers of Consonants " + countConsonants(str);
+}
+function countVowels(str) {
+    var vowel="aeiouAEIOU";
+    var vowelCount=0;
+    for(var x=0;x<str.length;x++){
+        for(var y=0;y<vowel.length;y++){
+            if(str[x]==vowel[y])
+            {
+                vowelCount++;
+            }
+        }
+    }
+    return vowelCount
+}
+function countConsonants(str) {
+    var consonant="bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ";
+    var consonantCount=0;
+    for(var x=0;x<consonant.length;x++){
+        for(var z=0;z<consonant.length;z++){
+            if(str[x]==consonant[z])
+            {
+                consonantCount++;
+            }
+        }
+    }
+    return consonantCount
 }
